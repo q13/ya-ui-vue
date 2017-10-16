7ae426d86d192cd1c446c50d6dbe76fe7b9555de
q13 <qsimon13@gmail.com>
2017-10-16 13:35:08 +0800

    MessageBox spell fixed.

--- pc/message-box.js
+++ pc/message-box.js
@@ -2,13 +2,13 @@ import Vue from 'vue';
 import './theme-default/base.css';
 import './theme-default/index.css';
 import './theme-default/message-box.css';
-import MesageBox from 'element-ui/lib/message-box';
+import MessageBox from 'element-ui/lib/message-box';
 import {
   replaceComponentPrefix
 } from '../src/deps/utils';
-Vue.component(replaceComponentPrefix(MesageBox.name, 'el-', 'y-'), MesageBox);
-Vue.prototype.$msgbox = MessageBox;
-Vue.prototype.$alert = MessageBox.alert;
-Vue.prototype.$confirm = MessageBox.confirm;
-Vue.prototype.$prompt = MessageBox.prompt;
+Vue.component(replaceComponentPrefix(MessageBox.name, 'el-', 'y-'), MesageBox);
+// Vue.prototype.$msgbox = MessageBox;
+// Vue.prototype.$alert = MessageBox.alert;
+// Vue.prototype.$confirm = MessageBox.confirm;
+// Vue.prototype.$prompt = MessageBox.prompt;
 export default MesageBox;
