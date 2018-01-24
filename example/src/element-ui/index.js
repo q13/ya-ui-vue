1f8e64d629a0777d30837b00a439d997590b44eb
q13 <qsimon13@gmail.com>
2018-01-24 17:57:45 +0800

    示例更新

--- example/src/element-ui/index.js
+++ example/src/element-ui/index.js
@@ -2,9 +2,15 @@
  * element-ui 示例
  */
 import '../../../ui/element-ui/themes/default/index.css';
-import '../../../ui/element-ui/components/button';
-import '../../../ui/element-ui/components/select';
-import '../../../ui/element-ui/components/option';
+import '../../../ui/element-ui/src/components/button';
+import '../../../ui/element-ui/src/components/select';
+import '../../../ui/element-ui/src/components/option';
+import '../../../ui/element-ui/src/components/time-picker';
+import '../../../ui/element-ui/src/components/table';
+import '../../../ui/element-ui/src/components/form';
+import '../../../ui/element-ui/src/components/form-item';
+import '../../../ui/element-ui/src/components/input';
+// import '../../../ui/element-ui/components/table-column';
 
 export default function () {
   return {
@@ -18,9 +24,110 @@ export default function () {
             :value="item.value">
           </el-option>
         </el-select>
+      <el-time-picker
+        is-range
+        v-model="value4"
+        range-separator="至"
+        value-format="HH:mm"
+        format="HH:mm"
+        start-placeholder="开始时间"
+        end-placeholder="结束时间"
+        placeholder="选择时间范围">
+      </el-time-picker>
+      <el-time-picker
+        v-model="value2"
+        value-format="HH:mm"
+        format="HH:mm"
+        placeholder="任意时间点">
+      </el-time-picker>
+      <el-table
+        :data="tableData"
+        style="width: 100%">
+        <el-table-column
+          prop="date"
+          label="日期"
+          width="180">
+        </el-table-column>
+        <el-table-column
+          prop="name"
+          label="姓名"
+          width="180">
+        </el-table-column>
+        <el-table-column
+          prop="address"
+          label="地址">
+        </el-table-column>
+      </el-table>
+      <el-form :model="ruleForm2" status-icon :rules="rules2" ref="ruleForm2" label-width="100px" class="demo-ruleForm">
+        <el-form-item label="密码" prop="pass">
+          <el-input type="password" v-model="ruleForm2.pass" auto-complete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="确认密码" prop="checkPass">
+          <el-input type="password" v-model="ruleForm2.checkPass" auto-complete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="年龄" prop="age">
+          <el-input v-model.number="ruleForm2.age"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="submitForm('ruleForm2')">提交</el-button>
+          <el-button @click="resetForm('ruleForm2')">重置</el-button>
+        </el-form-item>
+      </el-form>
     </div>`,
     data() {
+      var checkAge = (rule, value, callback) => {
+        if (!value) {
+          return callback(new Error('年龄不能为空'));
+        }
+        setTimeout(() => {
+          if (!Number.isInteger(value)) {
+            callback(new Error('请输入数字值'));
+          } else {
+            if (value < 18) {
+              callback(new Error('必须年满18岁'));
+            } else {
+              callback();
+            }
+          }
+        }, 1000);
+      };
+      var validatePass = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('请输入密码'));
+        } else {
+          if (this.ruleForm2.checkPass !== '') {
+            this.$refs.ruleForm2.validateField('checkPass');
+          }
+          callback();
+        }
+      };
+      var validatePass2 = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('请再次输入密码'));
+        } else if (value !== this.ruleForm2.pass) {
+          callback(new Error('两次输入密码不一致!'));
+        } else {
+          callback();
+        }
+      };
       return {
+        tableData: [{
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1517 弄'
+        }, {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1519 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }],
         options: [{
           value: '选项1',
           label: '黄金糕'
@@ -37,8 +144,43 @@ export default function () {
           value: '选项5',
           label: '北京烤鸭'
         }],
-        value: ''
+        value: '',
+        value2: new Date(2016, 9, 10, 18, 40),
+        value4: [new Date(2016, 9, 10, 8, 40), new Date(2016, 9, 10, 9, 40)],
+        ruleForm2: {
+          pass: '',
+          checkPass: '',
+          age: ''
+        },
+        rules2: {
+          pass: [
+            { validator: validatePass, trigger: 'blur' }
+          ],
+          checkPass: [
+            { validator: validatePass2, trigger: 'blur' }
+          ],
+          age: [
+            { required: function () {
+              return false;
+            }, trigger: 'blur' }
+          ]
+        }
       };
+    },
+    methods: {
+      submitForm(formName) {
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            alert('submit!');
+          } else {
+            console.log('error submit!!');
+            return false;
+          }
+        });
+      },
+      resetForm(formName) {
+        this.$refs[formName].resetFields();
+      }
     }
   };
 }
--- example/src/index.js
+++ example/src/index.js
@@ -1,20 +1,24 @@
 /**
+ * eslint-disable
+ */
+
+/**
  * 示例页
  */
 import 'babel-polyfill';
 import Vue from 'vue';
 import elementUi from './element-ui/index';
-import mintUi from './mint-ui/index';
-import iviewUi from './iview/index';
-import vueBeautyUi from './vue-beauty/index';
+// import mintUi from './mint-ui/index';
+// import iviewUi from './iview/index';
+// import vueBeautyUi from './vue-beauty/index';
 // console.log(elementUi);
 // import test from './test/index';
 // const Vue = window.Vue;
 const widgets = [
   elementUi(),
-  mintUi(),
-  iviewUi(),
-  vueBeautyUi()
+  // mintUi(),
+  // iviewUi(),
+  // vueBeautyUi()
   // test()
 ];
 const App = Vue.extend({
